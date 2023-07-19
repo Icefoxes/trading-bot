4badac7d19632041efe45e22fd074e6f6d6adb99
Kuo Guo <gary.guo.china@gmail.com>
2023-07-19 17:07:29 +0800

    Update shared.ts

--- hades-ui/src/services/shared.ts
+++ hades-ui/src/services/shared.ts
@@ -1,3 +1,3 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
 
-export const SiteBasicQuery = fetchBaseQuery({ baseUrl: 'http://localhost:8000/api/v1' });
+export const SiteBasicQuery = fetchBaseQuery({ baseUrl: process.env.HOST || 'http://localhost:8000/api/v1' });
